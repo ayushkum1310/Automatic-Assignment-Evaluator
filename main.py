24dfe1c557bfef4f32dd77b1d3467905d0f83c16
ayushkum1310 <ayushpripl@gmail.com>
2024-10-17 13:11:28 +0530

    Pass

--- main.py
+++ main.py
@@ -10,28 +10,28 @@ from src.text_extraction import TextExtractor,TextExtractorConfig
 
 if __name__=="__main__":
     try:
-        #Initializing current time
+        # Initializing current time
         start_time=time.time()
         
-        #Defining path of csv
+        # Defining path of csv
         pdf_path=Path('D:\END_TO_END_MAJOR\IMG_20240724_221056.pdf')
         
         file_name=pdf_path.stem
-        #Creating Data injestion object and Initiaing the function
+        # Creating Data injestion object and Initiaing the function
         Data_injestion_obj=DataIngestion()
         img_path,question=Data_injestion_obj.initiate_data_ingestion(pdf_path,"Will data company come for placement in shards")
         
-        #Creating Data Transformation object and Initiaing the function
+        # Creating Data Transformation object and Initiaing the function
         preprocessor_obj=DataTransformation()
         preprocesses_img=preprocessor_obj.initiate_data_transformation(img_path)
         
-        #Creating Data Extractor object and Initiaing the function
+        # Creating Data Extractor object and Initiaing the function
         text_extractor_obj=TextExtractor(file_name=file_name)
         text_file_path:list=[]
         extracted_tect_path=text_extractor_obj.initiate_text_extraction(preprocesses_img)
         text_file_path.append(extracted_tect_path)
         
-        #Printng The time required for The whole process
+        # Printng The time required for The whole process
         print(time.time()-start_time,'Time taken ')
         logging.info("The process of text Extraction and Document Making Has been completed")
         logging.info(f'Time taken to Digitize the Document is {time.time()-start_time} Sec')
